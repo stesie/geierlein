5e019952a8c46b6ec64f41646684d5f1b0dfeee9
Stefan Siegl <stesie@brokenpipe.de>
2012-04-09 19:11:53 +0200

    Add (yet mostly wip) XUL-based frontend.

---
+++ defaults/preferences/prefs.js
@@ -0,0 +1,8 @@
+pref("toolkit.defaultChromeURI", "chrome://geierlein/content/main.xul");
+
+/* debugging prefs */
+pref("browser.dom.window.dump.enabled", true);
+pref("javascript.options.showInConsole", true);
+pref("javascript.options.strict", true);
+pref("nglayout.debug.disable_xul_cache", true);
+pref("nglayout.debug.disable_xul_fastload", true);
