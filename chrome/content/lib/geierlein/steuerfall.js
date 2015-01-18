080748d7004ce59f7a0596da1f3317836807559f
Stefan Siegl <stesie@brokenpipe.de>
2015-01-18 20:28:28 +0100

    Bump version to 0.7.1

--- chrome/content/lib/geierlein/steuerfall.js
+++ chrome/content/lib/geierlein/steuerfall.js
@@ -114,7 +114,7 @@ geierlein.Steuerfall.prototype = {
                         datenteil.length.toString());
                     xml.writeElementString('TransportSchluessel', '');
                 xml.writeEndElement();  // Datei
-                xml.writeElementString('VersionClient', '0.7.0');
+                xml.writeElementString('VersionClient', '0.7.1');
             xml.writeEndElement();  // TransferHeader
         
             xml.writeStartElement('DatenTeil');
@@ -185,7 +185,7 @@ geierlein.Steuerfall.prototype = {
 
                 datenteil.writeStartElement('Hersteller');
                     datenteil.writeElementString('ProduktName', 'Geierlein');
-                    datenteil.writeElementString('ProduktVersion', '0.7.0');
+                    datenteil.writeElementString('ProduktVersion', '0.7.1');
                 datenteil.writeEndElement();
 
                 datenteil.writeStartElement('DatenLieferant')
