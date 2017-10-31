52ce4012f416c5a7c061ddaf8afafa2bed8f1eeb
Stefan Siegl <stesie@brokenpipe.de>
2017-10-31 18:24:18 +0100

    only show browser window if it's ready to show

--- chrome/app/main.js
+++ chrome/app/main.js
@@ -13,7 +13,9 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 800, height: 600, show: false})
+
+  mainWindow.once('ready-to-show', mainWindow.show);
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
