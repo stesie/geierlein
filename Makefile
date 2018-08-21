5347e754401bcc62c9f4af1cd38a6be42188b50a
Stefan Siegl <stesie@brokenpipe.de>
2018-08-21 18:57:47 +0200

    Bump version to 1.0.2

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ pkgdatadir := $(datadir)/geierlein
 
 VERSIONMAJOR := 1
 VERSIONMINOR := 0
-VERSIONBUILD := 1
+VERSIONBUILD := 2
 VERSION := $(VERSIONMAJOR).$(VERSIONMINOR).$(VERSIONBUILD)
 INSTALL := /usr/bin/install -c
 INSTALL_DATA := $(INSTALL) -m 644
@@ -45,7 +45,7 @@ autodist-run:
 bump-version: $(version_files)
 	@if [ "$(NEW_VERSION)" = "" ]; then \
 	  echo NEW_VERSION argument not provided.; \
-	  echo Usage: make bump-version NEW_VERSION=1.0.1; \
+	  echo Usage: make bump-version NEW_VERSION=1.0.2; \
 	  exit 1; \
 	fi
 	(bump_version() { \
--- chrome/content/lib/geierlein/steuerfall.js
+++ chrome/content/lib/geierlein/steuerfall.js
@@ -114,7 +114,7 @@ geierlein.Steuerfall.prototype = {
                         datenteil.length.toString());
                     xml.writeElementString('TransportSchluessel', '');
                 xml.writeEndElement();  // Datei
-                xml.writeElementString('VersionClient', '1.0.1');
+                xml.writeElementString('VersionClient', '1.0.2');
             xml.writeEndElement();  // TransferHeader
 
             xml.writeStartElement('DatenTeil');
@@ -185,7 +185,7 @@ geierlein.Steuerfall.prototype = {
 
                 datenteil.writeStartElement('Hersteller');
                     datenteil.writeElementString('ProduktName', 'Geierlein');
-                    datenteil.writeElementString('ProduktVersion', '1.0.1');
+                    datenteil.writeElementString('ProduktVersion', '1.0.2');
                 datenteil.writeEndElement();
 
                 datenteil.writeStartElement('DatenLieferant')
